449c80cf48d7adf5a21c98d55c9f7ffe40533655
CheruelQuentin <65130218+CheruelQuentin@users.noreply.github.com>
2023-03-14 11:10:31 +0100

    add jwt connexion

--- api/revendeur/routes/revendeur.routes.js
+++ api/revendeur/routes/revendeur.routes.js
@@ -3,5 +3,6 @@ const router = express.Router();
 
 const RevendeurController = require('../controllers/revendeur.controller')
 router.get('/',RevendeurController.allProducts)
+router.post('/login',RevendeurController.login)
 
 module.exports = router;
